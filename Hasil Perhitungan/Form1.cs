7792e0dc02ad6bba748d621f259676fbbd3cb6b2
Andre <ac4bfg@gmail.com>
2020-12-06 02:19:12 +0700

    Delete Collection

--- Hasil Perhitungan/Form1.cs
+++ Hasil Perhitungan/Form1.cs
@@ -12,7 +12,6 @@ namespace Hasil_Perhitungan
 {
     public partial class Form1 : Form
     {
-        private IList<Calculator> listOfCalculator = new List<Calculator>();
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +19,6 @@ namespace Hasil_Perhitungan
 
         private void FrmProses_OnCreate(Calculator cal)
         {
-            listOfCalculator.Add(cal);
-            ListBox item = new ListBox();
-
             Hasil.Items.Add(cal.NilaiHasil);
         }
         private void btnHitung_Click(object sender, EventArgs e)
